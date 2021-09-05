e53db90e15181e5dfd076b95ae4053e57bdf7b6d
Olukunle Oduwobi <ooduwobi@onefi.co>
2021-09-05 10:23:05 +0100

    - Set up Selenium webdriver with Chrome - Wrote first test to go to localhost - Wrote test to add first 2 items

---
+++ src/todo/Index.java
@@ -0,0 +1,56 @@
+package todo;
+
+import java.awt.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class Index {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+//		Go to localhost
+		driver.get("http://localhost:8080/");
+		
+		
+//		Wait for page to load
+		Thread.sleep(10000);
+		
+//		Verify that header is displayed
+		String Header = driver.findElement(By.xpath("/html/body/div[1]/div[1]/h1/text()")).getText();
+		Assert.assertEquals(Header, "Simple ToDo List ");
+		
+//		Add first Item 
+		driver.findElement(By.xpath("//*[@id=\"todo-form\"]/div/form/div/input")).sendKeys("Item 1");
+		
+//		Verify item was added 
+		String Item1 = driver.findElement(By.xpath("//*[@id=\"todo-list\"]/div/div/label/input")).getText();
+		Assert.assertEquals(Item1, "Item 1");
+		System.out.println("Item 1 added successfully");
+		
+//		Add second Item 
+		driver.findElement(By.xpath("//*[@id=\"todo-form\"]/div/form/div/input")).sendKeys("Item 2");
+		
+//		Verify item was added 
+		String Item2 = driver.findElement(By.xpath("//*[@id=\"todo-list\"]/div/div[2]/label/input")).getText();
+		Assert.assertEquals(Item2, "Item 2");
+		System.out.println("Item2 added successfully");
+		
+		
+		driver.quit();
+
+	}
+	
+	
+
+}
