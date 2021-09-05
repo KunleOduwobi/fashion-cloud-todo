7c02ddcc3299ea06532c832721752f91e9cf4d96
Olukunle Oduwobi <ooduwobi@onefi.co>
2021-09-05 12:28:40 +0100

    - Bug fixes

--- src/todo/Index.java
+++ src/todo/Index.java
@@ -18,56 +18,66 @@ public class Index {
 
 		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		WebDriver driver = new ChromeDriver();
-		
-//		Go to localhost
+
+		// Go to localhost
 		driver.get("http://localhost:8080/");
-		
-		
-//		Wait for page to load
+
+		// Wait for page to load
 		Thread.sleep(10000);
-		
-//		Verify that header is displayed
-		String Header = driver.findElement(By.xpath("/html/body/div[1]/div[1]/h1/text()")).getText();
-		Assert.assertEquals(Header, "Simple ToDo List ");
+
+		// Verify that header is displayed
+		String Header = driver.findElement(By.xpath("/html/body/div[1]/div[1]/h1")).getText();
+		Assert.assertEquals(Header, "Simple ToDo List 0");
 		System.out.println("Expected header found");
 
-		
-//		Add first Item 
+		// Add first Item
 		driver.findElement(By.xpath("//*[@id=\"todo-form\"]/div/form/div/input")).sendKeys("Item 1");
+		driver.findElement(By.xpath("//*[@id=\"todo-form\"]/div/form/button")).click();
 		Thread.sleep(2000);
 
-//		Verify item was added 
-		String Item1 = driver.findElement(By.xpath("//*[@id=\"todo-list\"]/div/div/label/input")).getText();
-		Assert.assertEquals(Item1, "Item 1");
+		// Verify first item was added
+		String Count = driver.findElement(By.xpath("/html/body/div[1]/div[1]/h1/span")).getText();
+		Assert.assertEquals(Count, "1");
 		System.out.println("Item 1 added successfully");
-		
-//		Add second Item 
+
+		// Add second Item
 		driver.findElement(By.xpath("//*[@id=\"todo-form\"]/div/form/div/input")).sendKeys("Item 2");
+		driver.findElement(By.xpath("//*[@id=\"todo-form\"]/div/form/button")).click();
 		Thread.sleep(2000);
 
-//		Verify item was added 
-		String Item2 = driver.findElement(By.xpath("//*[@id=\"todo-list\"]/div/div[2]/label/input")).getText();
-		Assert.assertEquals(Item2, "Item 2");
-		System.out.println("Item2 added successfully");
-		
-//		Remove second item 
+		// Verify item was added
+		Count = driver.findElement(By.xpath("/html/body/div[1]/div[1]/h1/span")).getText();
+		Assert.assertEquals(Count, "2");
+		System.out.println("Item 2 added successfully");
+
+		// Remove second item
 		driver.findElement(By.xpath("//*[@id=\"todo-list\"]/div/div[2]/label/input")).click();
 		Thread.sleep(2000);
 
-//		Verify that 2nd item was removed 	
+		// Verify that 2nd item was removed
 
-		java.util.List<WebElement> Items =driver.findElements(By.xpath("//input[@type='checkbox']"));
+		java.util.List<WebElement> Items = driver.findElements(By.xpath("//input[@type='checkbox']"));
+
+		int ItemsSize = Items.size();
+		Assert.assertEquals(ItemsSize, 1);
+		System.out.println("Item 2 removed successfully");
 		
-		int ItemsSize = Items.size(); 
-		Assert.assertEquals(ItemsSize, "1");
-		System.out.println("Item2 removed successfully");
+		// Remove first item
+		driver.findElement(By.xpath("//*[@id=\"todo-list\"]/div/div/label")).click();
+		Thread.sleep(2000);
 
-
+		// Verify that 2nd item was removed
+		Count = driver.findElement(By.xpath("/html/body/div[1]/div[1]/h1/span")).getText();
+		Assert.assertEquals(Count, "0");
+		System.out.println("Item 1 added successfully");
+		System.out.println("Item 1 removed successfully");
 		
+		System.out.println("Test Completed!");
+
+
+
 		driver.quit();
 
 	}
-	
-	
 
 }
